af9830d5a9539cfaddcb7e3a8fbae22fd84bf2c4
Sanggyu Lee <sanggyu523@naver.com>
2018-12-08 22:04:53 +0900

    Seperate pendulum

---
+++ Chaotic/pendulum.cpp
@@ -0,0 +1,9 @@
+//
+//  pendulum.cpp
+//  Chaotic
+//
+//  Created by 이상규 on 08/12/2018.
+//  Copyright © 2018 이상규. All rights reserved.
+//
+
+#include "pendulum.hpp"
---
+++ Chaotic/pendulum.hpp
@@ -0,0 +1,14 @@
+//
+//  pendulum.hpp
+//  Chaotic
+//
+//  Created by 이상규 on 08/12/2018.
+//  Copyright © 2018 이상규. All rights reserved.
+//
+
+#ifndef pendulum_hpp
+#define pendulum_hpp
+
+#include <stdio.h>
+
+#endif /* pendulum_hpp */
