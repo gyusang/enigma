53e22232a2f1e402dcc46fd5b1d6723a27e35787
jhm1313 <jhm1313@naver.com>
2018-12-09 15:51:07 +0900

    Add For loop

--- Chaotic/main.cpp
+++ Chaotic/main.cpp
@@ -19,9 +19,16 @@ int main()
     n = 2; l_0 = 10; theta_0 = 90;
     theta_0 = M_PI*theta_0/180;
 
-    unif01_Gen *gen = CreatePendulum(n, 10000, theta_0, l_0);
-    bbattery_SmallCrush(gen);
-    DeletePendulum(gen);
+	for (double i = 1; i <= 20; i++) {
+		unif01_Gen *gen = CreatePendulum(n, 10000, theta_0, i);
+		bbattery_SmallCrush(gen);
+		DeletePendulum(gen);
+	}
+	for (double i = M_PI / 4; i <= 2*M_PI/3; i += M_PI/24) {
+		unif01_Gen *gen = CreatePendulum(n, 10000, i, l_0);
+		bbattery_SmallCrush(gen);
+		DeletePendulum(gen);
+	}
     
     return 0;
 }
